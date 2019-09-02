c43f81c21cb68b6d986a897f01fcf3c303af2e3f
Felipe Bergamin <felipebergamin6@gmail.com>
2019-09-02 16:50:56 -0300

    class component && react state

---
+++ src/components/TechList.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+class TechList extends Component {
+  state = {
+    newTech: "",
+    techs: ["Node.JS", "ReactJS", "React Native"]
+  };
+
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ""
+    });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => (
+            <li key={tech}>{tech}</li>
+          ))}
+        </ul>
+
+        <input
+          type="text"
+          value={this.state.newTech}
+          onChange={this.handleInputChange}
+        />
+
+        <button type="submit">Enviar</button>
+      </form>
+    );
+  }
+}
+
+export default TechList;
